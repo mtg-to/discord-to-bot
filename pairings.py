6d1f96058fd06fd64edf8e449232c27fb58bba81
lukepatterson31 <62485951+lukepatterson31@users.noreply.github.com>
2020-04-02 08:33:00 +0100

    Add files via upload

--- pairings.py
+++ pairings.py
@@ -1,17 +1,9 @@
 import random
 
-
-#assigned pairings
-pairs = []
-
 def pair_random(players): 
-    if len(players)%2 != 0: players.append('None')
+    
     random.shuffle(players)
-
-    #create pairing tuples and add to pairings list
-    while len(players) > 1 :  
-        pairs.append((players[0], players [-1]))
-        players.pop(-1)
-   pairs = [ (c.pop(), c.pop()) for _ in range(len(c)//2) ]
-   if c: pairs.append((c.pop(), None))
+   
+    pairs = [(players.pop(),players.pop()) for _ in range(len(players)//2)]
+    if len(players)%2 != 0: pairs.append((players.pop(),None))
     return pairs
