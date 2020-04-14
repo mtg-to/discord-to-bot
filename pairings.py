f2430c36ceb34c0e22940a15ea18429370e5d3a6
lukepatterson31 <62485951+lukepatterson31@users.noreply.github.com>
2020-04-14 07:37:10 +0100

    Update pairings.py
    Co-Authored-By: Piotr Berlowski <piotrberlowski@users.noreply.github.com>

--- pairings.py
+++ pairings.py
@@ -5,5 +5,5 @@ def pair_random(players):
     random.shuffle(players)
    
     pairs = [(players.pop(),players.pop()) for _ in range(len(players)//2)]
-    if len(players)%2 != 0: pairs.append((players.pop(),None))
+    if players: pairs.append((players.pop(),None))
     return pairs
